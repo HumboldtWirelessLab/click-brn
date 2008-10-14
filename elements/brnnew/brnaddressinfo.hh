81c403fa459613d42cafa995b9bc1889d19fe449
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2008-10-14 17:02:03 +0200

    bla

--- elements/brnnew/brnaddressinfo.hh
+++ elements/brnnew/brnaddressinfo.hh
@@ -85,11 +85,10 @@ These defaults are not available on all platforms.
 
 PortInfo */
 
-
-#define ARPHRD_80211_PRISM     802       /* wifi + Prism2 header */
-#define ARPHRD_80211_RADIOTAP  803       /* wifi + Radiotap header */
-#define ARPHRD_80211_ATHDESC   804       /* wifi + Atheros header */
-#define ARPHRD_80211_ATHDESCEXT   805       /* wifi + Atheros header + X */
+#define ARPHRD_80211_PRISM	802       /* wifi + Prism2 header */
+#define ARPHRD_80211_RADIOTAP	803       /* wifi + Radiotap header */
+#define ARPHRD_80211_ATHDESC	804       /* wifi + Atheros header */
+#define ARPHRD_80211_ATHDESCEXT	805       /* wifi + Atheros header + X */
 
 class BRNAddressInfo : public Element { public:
   
