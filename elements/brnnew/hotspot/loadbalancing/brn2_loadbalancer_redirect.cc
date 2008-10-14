06a695f3c8b059b52929e6ca18c21fba33c83691
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2008-10-14 11:04:22 +0200

    test rerouting

--- elements/brnnew/hotspot/loadbalancing/brn2_loadbalancer_redirect.cc
+++ elements/brnnew/hotspot/loadbalancing/brn2_loadbalancer_redirect.cc
@@ -239,6 +239,7 @@ EtherAddress* LoadBalancerRedirect::getBestNodeForFlow(EtherAddress *srcEtherAdd
     if ( neighbors.size() > 0 )
     {
       int num = random() % neighbors.size();
+      num =  neighbors.size();
       if ( num == neighbors.size() )
         bestNode = &_me;
       else
