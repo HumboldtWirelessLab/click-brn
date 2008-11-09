64e2d05fe03ada3b5b9789ad2c47c94f5ec37681
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2008-11-09 20:19:38 +0100

    add type of error for wifi_extra->flags

---
+++ elements/brn/wifi/brnwifi.h
@@ -0,0 +1,9 @@
+#ifndef _BRNWIFI_H_
+#define _BRNWIFI_H_
+
+enum {
+  WIFI_EXTRA_RX_CRC_ERR		= (1<<31), /* failed crc check */
+  WIFI_EXTRA_RX_PHY_ERR   = (1<<30), /* failed phy check */
+};
+
+#endif /* !_BRNWIFI_H_ */
