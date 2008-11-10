a35024cec84c504b73d1c2d7be6224ac7869b245
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2008-11-10 09:59:02 +0100

    better support for trrates of madwifi

---
+++ elements/brn/wifi/ath/ath2encap.hh
@@ -0,0 +1,30 @@
+#ifndef CLICK_ATH2ENCAP_HH
+#define CLICK_ATH2ENCAP_HH
+#include <click/element.hh>
+#include <clicknet/ether.h>
+CLICK_DECLS
+
+class Ath2Encap : public Element { public:
+
+  Ath2Encap();
+  ~Ath2Encap();
+
+  const char *class_name() const  { return "Ath2Encap"; }
+  const char *port_count() const  { return PORTS_1_1; }
+  const char *processing() const  { return AGNOSTIC; }
+
+  int configure(Vector<String> &, ErrorHandler *);
+  bool can_live_reconfigure() const	{ return true; }
+
+  Packet *simple_action(Packet *);
+
+  void add_handlers();
+
+  bool _debug;
+
+ private:
+
+};
+
+CLICK_ENDDECLS
+#endif
