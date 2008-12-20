caf97ef564c37a0d14c6e93d131a1da3f02d4d3b
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2008-12-20 10:13:23 +0100

    bugfixes

--- elements/brn/wifi/brn2_setchannel.cc
+++ elements/brn/wifi/brn2_setchannel.cc
@@ -11,14 +11,15 @@ CLICK_DECLS
 
 BRN2SetChannel::BRN2SetChannel() :
     _rotate( false ),
-    _channel( 1 )
+    _channel( 0 )
 {
 }
 
 int
 BRN2SetChannel::configure(Vector<String> &conf, ErrorHandler *errh)
 {
-  if (cp_va_kparse(conf, this, errh, 
+  if (cp_va_kparse(conf, this, errh,
+      "CHANNEL", cpkN, cpInteger, &_channel,
       cpEnd) < 0)
     return -1;
 
@@ -29,11 +30,20 @@ Packet *
 BRN2SetChannel::simple_action(Packet *p_in)
 {
   uint8_t operation;
+  uint8_t set_channel;
 
   if ( p_in != NULL )
   {
     operation = BRNPacketAnno::operation_anno(p_in);
-    BRNPacketAnno::set_channel_anno(p_in, _channel, OPERATION_SET_CHANNEL_BEFORE_PACKET);
+    set_channel = BRNPacketAnno::channel_anno(p_in);
+
+    if ( ( set_channel == 0 ) && ( _channel != 0 ) )
+      BRNPacketAnno::set_channel_anno(p_in, _channel, OPERATION_SET_CHANNEL_BEFORE_PACKET);
+    else
+      if ( set_channel != 0 )
+        BRNPacketAnno::set_channel_anno(p_in, set_channel, OPERATION_SET_CHANNEL_BEFORE_PACKET);
+      else
+        BRNPacketAnno::set_channel_anno(p_in, _channel, OPERATION_SET_CHANNEL_BEFORE_PACKET);
   }
 
   return p_in;
