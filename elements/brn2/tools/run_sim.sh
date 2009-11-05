2ffaae4e034c870c4fee2915b6737adad0367235
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2009-11-05 12:26:17 +0100

    update for run_sim-tool

--- elements/brn2/tools/run_sim.sh
+++ elements/brn2/tools/run_sim.sh
@@ -4,10 +4,14 @@ SEDARG=""
 
 for i in `ls *.click`; do
   SEDARG="$SEGARG -e s#$i#$i.tmp#g"
-  DEBUG=$DEBUG ./click_dbg.sh $i $i.tmp
+  DEBUG=$DEBUG click_dbg.sh $i $i.tmp
 done
 
-TCLFILE=`ls *.tcl | tail -n 1 | awk '{print $1}'`
+if [ "x$1" = "x" ]; then
+  TCLFILE=`ls *.tcl | tail -n 1 | awk '{print $1}'`
+else
+  TCLFILE=$1
+fi
 
 cat $TCLFILE | sed $SEDARG > $TCLFILE.tmp
 
