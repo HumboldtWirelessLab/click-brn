28400632f794b784e2d61262db9562c5892ec56d
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2010-03-17 11:29:51 +0100

    Add FilterBSSID: element to filter frames for given bssid

--- elements/brn2/wifi/brnwifi.h
+++ elements/brn2/wifi/brnwifi.h
@@ -2,7 +2,7 @@
 #define _BRNWIFI_H_
 
 enum {
-  WIFI_EXTRA_RX_CRC_ERR		= (1<<31), /* failed crc check */
+  WIFI_EXTRA_RX_CRC_ERR	  = (1<<31), /* failed crc check */
   WIFI_EXTRA_RX_PHY_ERR   = (1<<30), /* failed phy check */
 };
 
