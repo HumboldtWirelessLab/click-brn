e09ba76981130a234fca4f4aa2c212027428cc9a
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2010-03-18 18:47:02 +0100

    BRN-Tools: update

--- elements/brn2/tools/brn-tools.sh
+++ elements/brn2/tools/brn-tools.sh
@@ -18,6 +18,10 @@ case "$SIGN" in
         ;;
 esac
 
+
+FULLFILENAME=`basename $0`
+FULLFILENAME=$DIR/$FULLFILENAME
+
 GITHOST=localhost
 
 git clone ssh://$GITHOST/home/sombrutz/repository/students/click-brn/.git
@@ -26,10 +30,26 @@ git clone ssh://$GITHOST/home/sombrutz/repository/click-brn-scripts.git
 
 (cd click-brn;touch ./configure; /bin/sh brn-conf.sh ns2_userlevel; make)
 (cd brn-ns2-click; VERSION=4 PREFIX=$DIR/ns2 CLICKPATH=$DIR/click-brn ./install_ns2.sh)
+(cd click-brn-scripts; ./build.sh)
 
 rm -rf $DIR/brn-ns2-click
 
 echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$DIR/click-brn/ns/:$DIR/ns2/lib" > $DIR/brn-tools.bashrc
 echo "export PATH=$DIR/ns2/bin/:$PATH" >> $DIR/brn-tools.bashrc
 
-echo "Use \"source $DIR/brn-tools.bashrc\" to setup the path-var or add stuff to .bashrc"
+cat $FULLFILENAME | grep "^#INFO" | sed -e "s/#INFO[[:space:]]*//g" -e "s#TARGETDIR#$DIR#g"
+
+exit 0
+
+#INFO
+#INFO
+#INFO --------------- FINISH ------------------
+#INFO
+#INFO
+#INFO
+#INFO
+#INFO Well done !
+#INFO
+#INFO Use "source TARGETDIR/brn-tools.bashrc" to setup the path-var or add stuff to .bashrc
+#INFO
+#INFO
