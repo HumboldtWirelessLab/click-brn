c6528f1846a8e8b0cb809ff059d77713d6095419
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2010-04-21 13:42:52 +0200

    RoutingPeek: also src and dst etheradd are given

--- elements/brn2/routing/routing_peek.hh
+++ elements/brn2/routing/routing_peek.hh
@@ -14,11 +14,11 @@ class RoutingPeek : public BRNElement
     class PeekFunction {
 
       public:
-        bool (*_peek_func)(void *, Packet*, int);  //Packet, brn_port
+        bool (*_peek_func)(void *, Packet*, EtherAddress *src, EtherAddress *dst, int);  //Packet, brn_port //TODO: add src and dst etheraddress
         void *_peek_obj;
         int _brn_port;
 
-        PeekFunction( bool (*peek_func)(void *, Packet* p, int brn_port), void *peek_obj, int brn_port ) {
+        PeekFunction( bool (*peek_func)(void *, Packet* p, EtherAddress *src, EtherAddress *dst, int brn_port), void *peek_obj, int brn_port ) {
           _peek_func = peek_func;
           _peek_obj = peek_obj;
           _brn_port = brn_port;
@@ -38,20 +38,27 @@ class RoutingPeek : public BRNElement
                                                   //virtual: späte Bindung
 
 
-    int add_routing_peek(bool (*peek_func)(void*, Packet*, int), void *peek_obj, int brn_port)
+    int add_routing_peek(bool (*peek_func)(void*, Packet*, EtherAddress *, EtherAddress *, int), void *peek_obj, int brn_port)
     {
       if ( ( peek_func == NULL ) || ( peek_obj == NULL ) ) return -1;
       _peeklist.push_back(new PeekFunction(peek_func, peek_obj, brn_port));
       return 0;
     }
 
-    void call_routing_peek(Packet *p, int brn_port) {
+    /**
+     * false: packet should not be forwarded
+     * true: nothing changed. forward the packet
+     */
+
+    bool call_routing_peek(Packet *p, EtherAddress *src, EtherAddress *dst, int brn_port) {
       PeekFunction *pf;
       for ( int i = 0; i < _peeklist.size(); i++ ) {
         pf = _peeklist[i];
         if ( pf->_brn_port == brn_port )
-          if ( ! (*pf->_peek_func)(pf->_peek_obj, p, brn_port) ) return;
+          if ( ! (*pf->_peek_func)(pf->_peek_obj, p, src, dst, brn_port) ) return false;
       }
+
+      return true;
     }
 
     virtual void add_handlers();
