c6a7bd6afda62396d8959905a430932749f6490f
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2010-12-17 16:03:12 +0100

    AlignmentTest: more tests and output

--- elements/brn2/tools/alignment-test.c
+++ elements/brn2/tools/alignment-test.c
@@ -12,13 +12,42 @@ int main(int argc, char *argv[]) {
     char buf[12];
     int i;
     int32_t value, try_value;
+    int32_t test = 0x12345678;
+
+    int32_t *p_test = &test;
+    char *p_c_test = (char*)p_test;
+    int32_t test_compare;
+
+    int32_t test_a[] = { 0x12345678, 0x9abcdef0 };
+
     get_value(buf, 0, &value);
     for (i = 1; i < 4; i++) {
         get_value(buf, i, &try_value);
-        printf("Check");
+        printf("Check\n");
         if (value != try_value)
         exit(1);
     }
+
+    p_c_test += 1;
+    p_test = (int32_t*)p_c_test;
+
+    printf("%x\n",test);
+    test_compare = *p_test;
+
+    printf("%x\n",test_compare);
+
+
+   printf("Start: %x %x\n",test_a[0], test_a[1]);
+   p_test = &test_a[0];
+   p_c_test = (char*)p_test;
+   for( i = 0; i < 4; i++ ) {
+     p_c_test += 1;
+     p_test = (int32_t*)p_c_test;
+     test_compare = *p_test;
+
+     printf("%d: %x\n",i,test_compare);
+   }
+
     exit(0);
 }
 
