88264e2dccfa8f8b500d2494d4fa18517085dc63
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2010-12-17 16:17:56 +0100

    AlignmentTest: alloc array using malloc for alignment test

--- elements/brn2/tools/alignment-test.c
+++ elements/brn2/tools/alignment-test.c
@@ -17,8 +17,12 @@ int main(int argc, char *argv[]) {
     int32_t *p_test = &test;
     char *p_c_test = (char*)p_test;
     int32_t test_compare;
-
     int32_t test_a[] = { 0x12345678, 0x9abcdef0 };
+
+    int32_t *test_b;
+    test_b = (int32_t*)malloc(2 * sizeof(int32_t));
+    test_b[0] = 0x12345678;
+    test_b[1] = 0x9abcdef0;
 
     get_value(buf, 0, &value);
     for (i = 1; i < 4; i++) {
@@ -39,6 +43,17 @@ int main(int argc, char *argv[]) {
 
    printf("Start: %x %x\n",test_a[0], test_a[1]);
    p_test = &test_a[0];
+   p_c_test = (char*)p_test;
+   for( i = 0; i < 4; i++ ) {
+     p_c_test += 1;
+     p_test = (int32_t*)p_c_test;
+     test_compare = *p_test;
+
+     printf("%d: %x\n",i,test_compare);
+   }
+
+   printf("Start_2: %x %x\n",test_b[0], test_b[1]);
+   p_test = &test_b[0];
    p_c_test = (char*)p_test;
    for( i = 0; i < 4; i++ ) {
      p_c_test += 1;
