bc0d9f579c98453c003dd43b9c1280416089a653
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2011-06-08 19:37:00 +0200

    GPSMap: mapping from etheraddress to GPSPosition (and reverse in the future). Used for georouting, nhopneighbourinfo, alarmingprotocol,...

---
+++ elements/brn2/services/sensor/gps/gps_map.hh
@@ -0,0 +1,40 @@
+#ifndef GPSMAP_HH
+#define GPSMAP_HH
+#include <click/element.hh>
+#include <click/ipaddress.hh>
+#include <click/etheraddress.hh>
+#include <click/hashmap.hh>
+
+#include "elements/brn2/brnelement.hh"
+
+#include "gps.hh"
+
+CLICK_DECLS
+
+class GPSMap : public BRNElement {
+
+ public:
+  GPSMap();
+  ~GPSMap();
+
+  const char* class_name() const { return "GPSMap"; }
+  void* cast(const char*);
+  int configure( Vector<String> &conf, ErrorHandler *errh );
+
+  GPSPosition* lookup(EtherAddress eth);
+
+  void remove(EtherAddress eth);
+  void insert(EtherAddress eth, GPSPosition gps);
+
+  typedef HashMap<EtherAddress, GPSPosition> EtherGPSMap;
+  typedef EtherGPSMap::const_iterator EtherGPSMapIter;
+
+  EtherGPSMap _map;
+
+  static String read_handler(Element *e, void *thunk);
+  void add_handlers();
+
+};
+
+CLICK_ENDDECLS
+#endif /* LEASETABLE_HH */
