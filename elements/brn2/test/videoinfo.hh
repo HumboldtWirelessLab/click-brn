1368871a0b6f1f9ba0d07c00d8ced19be7fd74e8
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2011-10-31 18:58:38 +0100

    Videoinfo: data from matlab video

---
+++ elements/brn2/test/videoinfo.hh
@@ -0,0 +1,67 @@
+/* Copyright (C) 2005 BerlinRoofNet Lab
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA. 
+ *
+ * For additional licensing options, consult http://www.BerlinRoofNet.de 
+ * or contact brn@informatik.hu-berlin.de. 
+ */
+
+#ifndef VIDEOINFOELEMENT_HH
+#define VIDEOINFOELEMENT_HH
+#include <click/element.hh>
+#include "elements/brn2/brnelement.hh"
+
+
+CLICK_DECLS
+
+class VideoInfoData {
+
+  public:
+    Timestamp _time;
+    String _data;
+
+    VideoInfoData(String data) {
+      _data = data;
+      _time = Timestamp::now();
+    }
+};
+
+typedef Vector<VideoInfoData> VideoInfoList;
+
+class VideoInfo : public BRNElement {
+
+  public:
+
+  VideoInfo();
+  ~VideoInfo();
+
+  const char *class_name() const  { return "VideoInfo"; }
+  const char *processing() const  { return AGNOSTIC; }
+
+  const char *port_count() const  { return "1/0"; }
+
+  int configure(Vector<String> &, ErrorHandler *);
+  void add_handlers();
+
+  void push(int, Packet *p);
+
+  String read_info();
+
+  VideoInfoList _vil;
+
+};
+
+CLICK_ENDDECLS
+#endif
