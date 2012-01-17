6a1416286f893e212fe189e2e6dea76649bac68e
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2012-01-17 18:40:52 +0100

    simclick: add command for read_position, read performance counter, set_cca

--- include/click/simclick.h
+++ include/click/simclick.h
@@ -107,6 +107,10 @@ int simclick_gettimeofday(struct timeval* tv);
 #define SIMCLICK_IF_PROMISC		12 // int ifid
 #define SIMCLICK_IPPREFIX_FROM_NAME	13 // const char *ifname, char *buf, int len
 
+#define SIMCLICK_GET_NODE_POSITION      20 // int *pos (4 int: x,y,z,speed)
+#define SIMCLICK_GET_PERFORMANCE_COUNTER 21 // int ifid, int *performance_counter
+#define SIMCLICK_CCA_OPERATION           22 // int ifid, int cca_operation, int cca_value
+
 int simclick_sim_command(simclick_node_t *sim, int cmd, ...);
 int simclick_click_command(simclick_node_t *sim, int cmd, ...);
 
