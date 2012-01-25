0725878cc9875a112ce8b15727c3c983a554840c
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2012-01-25 18:22:57 +0100

    Gateway: fix compiler errors

--- elements/brn2/services/gateway/brnsetgateway.cc
+++ elements/brn2/services/gateway/brnsetgateway.cc
@@ -184,11 +184,10 @@ BRNSetGateway::choose_gateway() {
 }
 
 /**
- * 
+ *
  * Chooses a gateway and rewrites the packet
- * 
+ *
  */
-#WARNING gw per reference uebergeben
 void
 BRNSetGateway::set_gateway_on_packet(Packet *p_in, const EtherAddress *gw) {
 
