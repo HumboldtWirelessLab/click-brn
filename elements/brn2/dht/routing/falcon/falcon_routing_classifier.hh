716a5c55af6dec93af7606e788c7b60e2f4ae92a
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2012-02-13 10:47:47 +0100

    Falcon: passive monitoring update

--- elements/brn2/dht/routing/falcon/falcon_routing_classifier.hh
+++ elements/brn2/dht/routing/falcon/falcon_routing_classifier.hh
@@ -8,7 +8,8 @@ CLICK_DECLS
 #define FALCON_ROUTING_POSITION_PORT 1
 #define FALCON_ROUTING_LEAVE_PORT    2
 #define FALCON_NETWORKSIZE_PORT      3
-#define FALCON_ROUTING_UNKNOWN_PORT  4
+#define FALCON_PASSIVE_MONITOR_PORT  4
+#define FALCON_ROUTING_UNKNOWN_PORT  5
 
 /**
  * TODO: Can be replaced by simple classifier
@@ -25,7 +26,7 @@ class FalconRoutingClassifier : public Element
 
     const char *processing() const  { return PUSH; }
 
-    const char *port_count() const  { return "1/4-5"; }
+    const char *port_count() const  { return "1/5-6"; }
 
     int configure(Vector<String> &, ErrorHandler *);
     bool can_live_reconfigure() const  { return false; }
