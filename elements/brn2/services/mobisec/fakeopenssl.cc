6965e5e194785d30b495896a4f3e0091c493c5cc
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2012-05-30 19:41:57 +0200

    Fix openssl stuff

--- elements/brn2/services/mobisec/fakeopenssl.cc
+++ elements/brn2/services/mobisec/fakeopenssl.cc
@@ -3,19 +3,7 @@
 
 #include "fakeopenssl.hh"
 
-#if HAVE_OPENSSL
-
-#include <click/element.hh>
-#include <click/confparse.hh>
-
-#include "elements/brn2/brnelement.hh"
-#include "elements/brn2/standard/brnlogger/brnlogger.hh"
-#include "elements/brn2/brn2.h"
-#include "elements/brn2/brnprotocol/brnpacketanno.hh"
-#include "elements/brn2/brnprotocol/brnprotocol.hh"
-
-#include "kdp.hh"
-#include "keymanagement.hh"
+#if defined(HAVE_OPENSSL)
 
 CLICK_DECLS
 
@@ -27,6 +15,7 @@ FakeOpenSSL::~FakeOpenSSL() {
 }
 
 CLICK_ENDDECLS
+ELEMENT_REQUIRES(userlevel|ns)
 ELEMENT_PROVIDES(FakeOpenSSL)
 ELEMENT_LIBS(-lssl -lcrypto)
 
