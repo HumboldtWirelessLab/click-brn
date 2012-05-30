3e2f2ff15743673ac2c31459bc46c27eaed6c249
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2012-05-30 19:41:57 +0200

    Fix openssl stuff

--- elements/brn2/services/mobisec/fakeopenssl.hh
+++ elements/brn2/services/mobisec/fakeopenssl.hh
@@ -3,23 +3,12 @@
 
 #if HAVE_OPENSSL
 
-#include <string>
-#include <time.h>
-
-#include <click/element.hh>
-#include <click/confparse.hh>
-#include <click/hashmap.hh>
-#include "elements/brn2/brnelement.hh"
-
 CLICK_DECLS
 
-class FakeOpenSSL : public BRNElement {
+class FakeOpenSSL {
 public:
   FakeOpenSSL();
   ~FakeOpenSSL();
-
-  const char *class_name() const { return "FakeOpenSSL"; }
-
 };
 
 CLICK_ENDDECLS
