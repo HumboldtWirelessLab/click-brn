21b7c82c727cf7a71578c78b9b4d3a1cecd0b21e
Dominik Oepen <oepen@informatik.hu-berlin.de>
2012-06-05 14:55:13 +0200

    Initial commit of ShamirServer

---
+++ elements/brn2/services/shamir/ShamirServer.cc
@@ -0,0 +1,196 @@
+/*
+ * ShamirServer.cc
+ *
+ *  Created on: 08.05.2012
+ *      Author: Dominik Oepe
+ *
+ */
+
+#include <click/config.h>
+#include <click/element.hh>
+#include <click/confparse.hh>
+#include <click/packet.hh>
+
+#include <iostream>
+#include <sstream>
+
+#include "elements/brn2/brnelement.hh"
+#include "elements/brn2/standard/brnlogger/brnlogger.hh"
+#include "elements/brn2/brn2.h"
+#include "elements/brn2/brnprotocol/brnpacketanno.hh"
+#include "elements/brn2/brnprotocol/brnprotocol.hh"
+
+#include "ShamirServer.hh"
+
+#include <openssl/opensslv.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+CLICK_DECLS
+
+ShamirServer::ShamirServer()
+	: _debug(false)
+{
+	BRNElement::init(); // what for??
+}
+
+TLS::~TLS() {
+	SSL_shutdown(conn);
+	ERR_free_strings();
+	SSL_CTX_free(ctx);
+	SSL_free(conn); // frees also BIOs, cipher lists, SSL_SESSION
+	BIO_free(bio_err);
+}
+
+
+int ShamirServer::configure(Vector<String> &conf, ErrorHandler *errh) {
+
+	if (cp_va_kparse(conf, this, errh,
+		"DEBUG", cpkP, cpInteger, /*"Debug",*/ &_debug,
+		cpEnd) < 0)
+		return -1;
+
+	return 0;
+}
+
+void ShamirServer::push(int port, Packet *p) {
+
+	if (port == 0) { // data from network
+		BRN_DEBUG("Received Shamir request");
+		handle_request(p);
+	} else {
+		BRN_DEBUG("port %d: oops !!", port, p->length());
+		p->kill();
+	}
+}
+
+/*
+ * *******************************************************
+ *               private functions
+ * *******************************************************
+ */
+
+int handle_request(Packet *p) {
+
+    shamir_reply reply;
+    reply->share_id = _share_id;
+    reply->share_length = BN_bn2bin(_share, reply->share);
+    if (!reply->share_length) {
+        BRN_DEBUG("Failed to handle request");
+        p->kill();
+        return -1;
+    }
+
+    WriteablePacket *reply = Packet::make(128, NULL, 2*sizeof(uint32_t)+reply->share_length);
+    return 0;
+}
+
+/*
+ * *******************************************************
+ *               extra functions
+ * *******************************************************
+ */
+
+enum {
+    H_MODULUS,
+    H_SHARE,
+    H_SHARE_ID
+};
+
+static String read_param(Element *e, void *thunk) {
+	ShamirServer *shamir_server = (ShamirServer *)e;
+    char *c = NULL;
+    string ret;
+
+    switch((intptr_t) vparam) {
+    case H_MODULUS:
+        {
+            c = BN_bn2hex(shamir_server->_modulus)
+            s << c;
+            free(c);
+            break;
+        }
+    case H_SHARE:
+        {
+            c = BN_bn2hex(shamir_server->_share)
+            s << c;
+            free(c);
+            break;
+        }
+    case H_SHARE_ID:
+        {
+            s << shamir_server->_share_id;
+            break;
+        }
+    default:
+        {
+            BRN_DEBUG("Invalid call to write handler");
+        }
+    }
+
+    return s;
+}
+
+static int write_param(const String &in_s, Element *e, void *vparam,
+            ErrorHandler *errh) {
+    ShamirServer *s = (ShamirServer) *e;
+
+    switch((intptr_t) vparam) {
+        case H_MODULUS:
+            {
+                BIGNUM *bn = NULL;
+                if (!BN_hex2bn(&bn, in_s->c_str()))
+                    BRN_DEBUG("Invalid call to write handler");
+                }
+                if (s->_modulus)
+                    BN_free(s->_modulus);
+                s->_modulus = BN_dup(bn);
+                BN_free(bn);
+                break;
+            }
+        case H_SHARE:
+            {
+                BIGNUM *bn = NULL;
+                if (!BN_hex2bn(&bn, in_s->c_str()))
+                    BRN_DEBUG("Invalid call to write handler");
+                }
+                if (s->_share)
+                    BN_free(s->_share);
+                s->_share = BN_dup(bn);
+                BN_free(bn);
+                break;
+            }
+        case H_SHARE_ID:
+            {
+                unsigned int id;
+                stringstream ss(in_s);
+
+                if ((ss >> id).fail()) {
+                    BRN_DEBUG("Invalid call to write handler");
+                    break;
+                }
+                s->_share_id = id;
+                break;
+            }
+        default:
+            {
+                BRN_DEBUG("Invalid call to write handler");
+            }
+    return 0;
+}
+
+void ShamirServer::add_handlers()
+{
+  BRNElement::add_handlers();
+
+  add_read_handler("modulus", handler_triggered_handshake, NULL);
+  add_write_handler("modulus", handler_triggered_handshake, NULL);
+  add_read_handler("share", handler_triggered_handshake, NULL);
+  add_write_handler("share", handler_triggered_handshake, NULL);
+  add_read_handler("share_id", handler_triggered_handshake, NULL);
+  add_write_handler("share_id", handler_triggered_handshake, NULL);
+}
+
+CLICK_ENDDECLS
+//EXPORT_ELEMENT(TLS)
+ELEMENT_LIBS(-lssl -lcrypto)
---
+++ elements/brn2/services/shamir/ShamirServer.hh
@@ -0,0 +1,60 @@
+/*
+ * tls.hh
+ *
+ *  Created on: 18.04.2012
+ *      Author: aureliano
+ */
+
+#ifndef SHAMIR_SERVER_HH_
+#define SHAMIR_SERVER_HH_
+
+#include <string>
+#include <queue>
+
+#include <click/element.hh>
+#include <click/confparse.hh>
+#include <click/hashmap.hh>
+#include <click/packet.hh>
+#include "elements/brn2/brnelement.hh"
+
+#include <openssl/bn.h>
+
+
+using namespace std;
+
+CLICK_DECLS
+
+struct shamir_reply {
+    unsigned int share_id;
+    unsigned int share_len;
+    unsigned char *share;
+}
+
+class ShamirServer : public BRNElement {
+public:
+	ShamirServer();
+	~ShamirServer();
+
+	const char *class_name() const { return "ShamirServer"; }
+	const char *port_count() const { return "2/2"; }
+	const char *processing() const { return PUSH; }
+	void push(int port, Packet *p);
+
+	int configure(Vector<String> &conf, ErrorHandler *errh);
+	bool can_live_reconfigure() const	{ return false; }
+	int initialize(ErrorHandler* errh);
+
+	void add_handlers();
+private:
+    BN_CTX *_bn_ctx;
+    BIGNUM *_modulus;
+    BIGNUM *_share;
+    unsigned int _share_id;
+
+	int _debug;
+
+    int handle_request(Packet *p);
+};
+
+CLICK_ENDDECLS
+#endif /* SHAMIR_SERVER_HH_ */
