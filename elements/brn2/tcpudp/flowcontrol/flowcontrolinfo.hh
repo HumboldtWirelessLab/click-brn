761d6c07a53e974fa7db204f17f46a4e9e8c0377
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2012-09-01 03:07:23 +0200

    Flowcontrol udate

--- elements/brn2/tcpudp/flowcontrol/flowcontrolinfo.hh
+++ elements/brn2/tcpudp/flowcontrol/flowcontrolinfo.hh
@@ -12,34 +12,53 @@ CLICK_DECLS
 
 
 class FlowControlInfo {
-  
+
   public:
 
     FlowControlInfo();
-    FlowControlInfo(uint16_t max_window_size);
+    FlowControlInfo(uint16_t flowid);
+    FlowControlInfo(uint16_t flowid, uint16_t max_window_size);
     ~FlowControlInfo();
 
     Packet **_packet_window;
     bool *_ack_window;
-    
+    Timestamp *_packet_times;
+
+    uint16_t _flowid;
+
     uint16_t _max_window_size;
     uint16_t _cur_window_size;
-    
+
     uint16_t _window_start_index;
     uint16_t _window_next_index;
-    
+
     uint16_t _min_seq;
     uint16_t _next_seq;
 
     uint32_t _rtt;
 
+    int insert_packet(Packet *p);
+
+    bool has_packet(uint16_t seq);
     int insert_packet(Packet *p, uint16_t seq);
-    
-    uint32_t non_acked_packets();
+
+    bool packet_fits_in_window(uint16_t seq);
+    Packet* get_and_clear_acked_seq();
+
+    int32_t max_not_acked_seq();
+
+    void clear_packets_up_to_seq(uint16_t seq); //source
 
     void clear();
 
+    int32_t min_age_not_acked();
+    Packet *get_packet_with_max_age(int32_t age);
+
+    String print_info();
 };
+
+typedef HashMap<uint16_t, FlowControlInfo*> FlowTable;
+typedef FlowTable::const_iterator FTIter;
 
 CLICK_ENDDECLS
 #endif
