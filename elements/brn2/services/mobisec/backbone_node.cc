ab122a74d9d89d7a22846aa354dacac0f42d9929
Christian Ricardo Kühne <kuehne@informatik.hu-berlin.de>
2012-09-24 21:02:50 +0200

    backbone_node: change kdp-retry; change switch_dev()

--- elements/brn2/services/mobisec/backbone_node.cc
+++ elements/brn2/services/mobisec/backbone_node.cc
@@ -19,6 +19,8 @@
 #include "kdp.hh"
 #include "backbone_node.hh"
 
+#define RETRY_CNT_DOWN 1
+
 CLICK_DECLS
 
 BACKBONE_NODE::BACKBONE_NODE()
@@ -46,12 +48,16 @@ int BACKBONE_NODE::configure(Vector<String> &conf, ErrorHandler *errh) {
 		"WEPENCAP", cpkP+cpkM, cpElement, &_wepencap,
 		"WEPDECAP", cpkP+cpkM, cpElement, &_wepdecap,
 		"TLS", cpkP+cpkM, cpElement, &_tls,
-		"ASSOCREQ", cpkP+cpkM, cpElement, &_assocreq,
+
+		// BEGIN elements to control network stack
+		"DEV_CLIENT", cpkP+cpkM, cpElement, &_wifidev_client,
 		"AP_Q", cpkP+cpkM, cpElement, &_ap_q,
 		"CLIENT_Q", cpkP+cpkM, cpElement, &_client_q,
 		"DEVICE_CONTROL_UP", cpkP, cpElement, &_dev_control_up,
 		"DEVICE_CONTROL_DOWN", cpkP, cpElement, &_dev_control_down,
 		"DEVICE_CONTROL_DOWN2", cpkP, cpElement, &_dev_control_down2,
+		// END elements to control network stack
+
 		"DEBUG", cpkP, cpInteger, /*"Debug",*/ &_debug,
 		cpEnd) < 0)
 		return -1;
@@ -78,13 +84,14 @@ int BACKBONE_NODE::initialize(ErrorHandler *) {
 	BRN_DEBUG("random number: %i", randnum);
 
 	req_id = 0;
-	last_req_try = 0;
 
 	// As mentioned in the MobiSEC-Paper, but defined by myself
 	tolerance = _key_timeout * 1.5 + randnum;
 
-	// Time between retries of kdp request transmission
-	backoff = _key_timeout * 0.5 + randnum;
+	// Time between retries of kdp request transmission (Todo: Find a reasonable
+	// time for backoff, NOT defined by MobiSEC-paper)
+	backoff = _key_timeout * 0.5;
+	retry_cnt_down = RETRY_CNT_DOWN;
 
 	// Configuration determines some crypto parameters
 	keyman.set_key_timeout(_key_timeout);
@@ -99,7 +106,7 @@ int BACKBONE_NODE::initialize(ErrorHandler *) {
 
 	switch_dev(dev_client);
 
-	// Set stat variables
+	// Set statistical variables
 	bb_join_cnt = 0;
 	kdp_retry_cnt = 0;
 	key_inst_cnt = 0;
@@ -117,24 +124,28 @@ void BACKBONE_NODE::push(int port, Packet *p) {
 	}
 }
 
+/*
+ * To understand the inner workings of snd_kdp_req() and handle_kdp_reply()
+ * please consider to read both functions, having in mind, that
+ * they also remain on a third component: the timers!
+ * The timers are responsible for the correct time-dependent execution, while
+ * handle_kdp_reply() might reschedule snd_kdp_req().
+ */
 void BACKBONE_NODE::snd_kdp_req() {
 	/*
-	 * Turn off WEP temporarily, if we can't receive a kdp-reply.
+	 * Switch to client_dev (fall back), if we can't receive a kdp-reply.
 	 * Reason: Packets might be wep-encrypted wrong due to
-	 * missing or wrong keys. Therefore wep must be switched off
-	 * temporarily.
+	 * missing or wrong keys. Therefore we switch back to client_dev
 	 */
-	if (Timestamp::now().msecval()-last_req_try < backoff*1.5) {// If request was send a little time ago, retry without wep
-
+	if (retry_cnt_down <= 0) {
 		BRN_DEBUG("Retry kdp process...");
 		kdp_retry_cnt++;
 
 		switch_dev(dev_client);
 
 		HandlerCall::call_read(_tls, "restart", NULL);
-
-	} else { // If not, then we are about to send our first request for next epoch data
-		last_req_try = Timestamp::now().msecval();
+	} else {
+		retry_cnt_down--;
 	}
 
 	WritablePacket *p = kdp::kdp_request_msg();
@@ -147,9 +158,9 @@ void BACKBONE_NODE::snd_kdp_req() {
 	BRN_DEBUG("Sending KDP-Request...");
 	output(0).push(p);
 
-	/* Begin of "resend mechanism" in case of no response */
-	// Todo: Find a reasonable time for backoff
-	kdp_timer.schedule_after_msec(backoff);
+	// Begin of "resend mechanism" in case of no response.
+	// This loop will terminate, if we receive our kdp reply (see handle_kdp_reply).
+	kdp_timer.schedule_after_msec(backoff + tolerance);
 }
 
 void BACKBONE_NODE::handle_kdp_reply(Packet *p) {
@@ -194,12 +205,13 @@ void BACKBONE_NODE::handle_kdp_reply(Packet *p) {
 	// the first try, we are very close to enter the new epoche. If it takes longer to install
 	// the keys than we probably missed the entry point in time and have to wait until the actual
 	// session passes.
-	int anticipation = 200;
+	int anticipation = 200; // This is important to prevent a dangerous switch while installing new epoch data.
 	epoch_timer.schedule_at(Timestamp::make_msec(BUF_keyman.get_validity_start_time() - anticipation));
 
 	// Set timer to send next kdp-req
 	int keylist_livetime = _key_timeout*BUF_keyman.get_cardinality();
 	kdp_timer.reschedule_at(Timestamp::make_msec(BUF_keyman.get_validity_start_time() + keylist_livetime - tolerance));
+	retry_cnt_down = RETRY_CNT_DOWN; // reset retry countdown
 
 	p->kill();
 }
@@ -225,11 +237,12 @@ void BACKBONE_NODE::jmp_next_session() {
 void BACKBONE_NODE::jmp_next_epoch() {
 	// Switch to new epoch (copy new epoch data from BUF_keyman to keyman)
 
-	keyman.set_ctrl_data( BUF_keyman.get_ctrl_data() );
-
-	keyman.install_keylist( BUF_keyman.get_keylist() );
-
-	BRN_DEBUG("Switched to new epoch");
+	if ( keyman.set_ctrl_data( BUF_keyman.get_ctrl_data() ) ) {
+		keyman.install_keylist( BUF_keyman.get_keylist() );
+		BRN_DEBUG("Switched to new epoch");
+	} else {
+		BRN_DEBUG("Jump to next epoch failed due to wrong control data.");
+	}
 }
 
 /*
@@ -240,15 +253,15 @@ void BACKBONE_NODE::jmp_next_epoch() {
 
 void BACKBONE_NODE::switch_dev(enum dev_type type) {
 	String type_str;
-	String port;
+	String network_stack_nr;
 
 	switch(type) {
 	case dev_ap:
-		port = "1";
+		network_stack_nr = "1";
 		type_str = "dev_ap";
 		break;
 	case dev_client:
-		port = "0";
+		network_stack_nr = "0";
 		type_str = "dev_client";
 		break;
 	default:
@@ -257,7 +270,7 @@ void BACKBONE_NODE::switch_dev(enum dev_type type) {
 	}
 
 	String curr_port = HandlerCall::call_read(_dev_control_up, "switch", NULL);
-	if(curr_port == port) {
+	if(curr_port == network_stack_nr) {
 		BRN_DEBUG("Using same dev: %s", type_str.c_str());
 		return;
 	} else {
@@ -266,20 +279,27 @@ void BACKBONE_NODE::switch_dev(enum dev_type type) {
 		// Both devices are functioning although the flow only runs through one device.
 		// So when switching the device its queue is possibly overflowed. Thus we
 		// have to reset the queues to get a clean start.
-		HandlerCall::call_write(_ap_q, "reset", port, NULL);
-		HandlerCall::call_write(_client_q, "reset", port, NULL);
+		HandlerCall::call_write(_ap_q, "reset", network_stack_nr, NULL);
+		HandlerCall::call_write(_client_q, "reset", network_stack_nr, NULL);
+	}
 
-		if (port == "1") {
-			BRN_DEBUG("Sending disassoc to abandon client status");
-			HandlerCall::call_write(_assocreq, "send_disassoc_req", NULL);
-		}
+	if (network_stack_nr == "1") {
+		BRN_DEBUG("Sending disassoc to abandon client status");
+		HandlerCall::call_write(_wifidev_client, "disassoc", NULL);
 	}
 
 	// Tell the Click-Switch to switch and thus let the packet flow go through the
 	// other device.
-	HandlerCall::call_write(_dev_control_up, "switch", port, NULL);
-	HandlerCall::call_write(_dev_control_down, "switch", port, NULL);
-	HandlerCall::call_write(_dev_control_down2, "switch", port, NULL);
+	HandlerCall::call_write(_dev_control_up, "switch", network_stack_nr, NULL);
+	HandlerCall::call_write(_dev_control_down, "switch", network_stack_nr, NULL);
+	HandlerCall::call_write(_dev_control_down2, "switch", network_stack_nr, NULL);
+
+	if (network_stack_nr == "0") {
+		BRN_DEBUG("Sending assoc to next ap");
+		HandlerCall::call_write(_wifidev_client, "do_assoc", NULL);
+	}
+
+	BRN_DEBUG("Switching completed.");
 }
 
 
@@ -294,9 +314,15 @@ String BACKBONE_NODE::stats() {
 	  return sa.take_string();
 }
 
+void BACKBONE_NODE::reset_key_material() {
+	// Set the validity start time of keys to 0 in order to make them expire.
+	keyman.set_validity_start_time(0);
+	BRN_DEBUG("reset key material");
+}
 
 
-enum {H_SND_KDP_REQ, H_STATS};
+
+enum {H_SND_KDP_REQ, H_STATS, H_RST_KEYS};
 
 static String backbone_read_param(Element *e, void *thunk) {
 	BACKBONE_NODE *bn = (BACKBONE_NODE *)e;
@@ -304,6 +330,10 @@ static String backbone_read_param(Element *e, void *thunk) {
 	switch ((uintptr_t) thunk) {
 	case H_SND_KDP_REQ: {
 		bn->snd_kdp_req();
+		return String();
+	}
+	case H_RST_KEYS: {
+		bn->reset_key_material();
 		return String();
 	}
 	case H_STATS: {
@@ -318,6 +348,7 @@ void BACKBONE_NODE::add_handlers() {
 	BRNElement::add_handlers();
 
 	add_read_handler("snd_kdp_request", backbone_read_param, (void *)H_SND_KDP_REQ);
+	add_read_handler("reset_key_material", backbone_read_param, (void *)H_RST_KEYS);
 	add_read_handler("stats", backbone_read_param, (void *)H_STATS);
 }
 
