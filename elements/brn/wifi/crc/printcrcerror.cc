26da09712b7c4939a348253c9e0fb3fc09c65f17
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2013-03-26 17:33:30 +0100

    PrintCRC: compare paket with zeros (e.g. simpleflow)

--- elements/brn/wifi/crc/printcrcerror.cc
+++ elements/brn/wifi/crc/printcrcerror.cc
@@ -4,12 +4,18 @@
 #include <click/straccum.hh>
 #include <click/packet_anno.hh>
 #include <clicknet/wifi.h>
+
 #include "printcrcerror.hh"
+#include <elements/brn/wifi/brnwifi.hh>
 
 CLICK_DECLS
 
 PrintCRCError::PrintCRCError():
-  _rate(0)
+  _rate(0),
+  _offset(0),
+  _analyse(false),
+  _bits(1),
+  _pad(0)
 {
 }
 
@@ -20,9 +26,15 @@ PrintCRCError::~PrintCRCError()
 int
 PrintCRCError::configure(Vector<String> &conf, ErrorHandler* errh)
 {
+  _label = "";
+
   if (cp_va_kparse(conf, this, errh,
-      "LABEL", cpkP+cpkM, cpString, /*"label",*/ &_label,
-      "RATE", cpkP+cpkM, cpInteger, /*"rate",*/ &_rate,
+      "LABEL", cpkP+cpkM, cpString, &_label,
+      "RATE", cpkP+cpkM, cpInteger, &_rate,
+      "OFFSET", cpkP, cpInteger, &_offset,
+      "ANALYSE", cpkP, cpBool, &_analyse,
+      "BITS", cpkP, cpInteger, &_bits, 
+      "PAD", cpkP, cpInteger, &_pad, 
       cpEnd) < 0)
        return -1;
   return 0;
@@ -38,39 +50,52 @@ Packet *
 PrintCRCError::simple_action(Packet *p_in)
 {
   uint8_t *packet_data = (uint8_t*)p_in->data();
-  uint32_t i;
-  int crc_count;
   uint8_t next_byte;
+  uint32_t seq_num, bit_pos, crc_count;
+
   StringAccum sa;
-  uint32_t seq_num;
-  uint32_t bit_pos;
-  struct click_wifi_extra *ceh = (struct click_wifi_extra *) p_in->anno();
 
-  if ( ( _rate == 0 ) || ( ( _rate != 0 ) && ( _rate == ceh->rate ) ) )
-  {
-    memcpy(&seq_num, &(packet_data[2]), sizeof(seq_num));
+  struct click_wifi_extra *ceh = (struct click_wifi_extra *)WIFI_EXTRA_ANNO(p_in);
 
-    seq_num=ntohl(seq_num);
+  if ( (ceh->flags & WIFI_EXTRA_RX_CRC_ERR) && ((_rate == 0) || ( _rate == ceh->rate)) ) {
+    
+    
+    if ( _label != "" ) {
+      memcpy(&seq_num, &(packet_data[2]), sizeof(seq_num));
+      seq_num=ntohl(seq_num);
+
+      sa << _label << ": " << (uint32_t)ceh->rate << " " << seq_num << " " << p_in->length();
+    }
 
     crc_count = 0;
     bit_pos = 0;
 
-    for ( i = 4; i < p_in->length(); i++)
+    int i;
+    for ( i = _offset; i < p_in->length(); i++)
     {
+      //OMG: This code is pretty ugly.
       next_byte = packet_data[i];
-      if ( next_byte != 0 ) crc_count++;
+      if ( next_byte != 0 ) {
+        crc_count++;
 
-      for(uint8_t bit = 128; bit > 0; bit = bit >> 1)
-      {
-        sa << ceh->rate;
-        sa << " ";
+        if ( _bits == 8 ) sa << " 1";
 
-        sa << seq_num << " " << bit_pos;
-        if ( ( next_byte & bit ) == 1 ) sa << " 1\n";
-        else sa << " 0\n";
-        bit_pos++;
+      } else if ( _bits == 8 ) sa << " 0";
+
+      if ( _bits == 1 ) {
+        for(uint8_t bit = 128; bit > 0; bit = bit >> 1)
+        {
+          if ( ( next_byte & bit ) == 1 ) sa << " 1";
+          else sa << " 0";
+          bit_pos++;
+        }
       }
+      
     }
+    
+    if ( _pad != 0 )
+      for ( ; i < _pad; i++) sa << " 9";
+
     click_chatter("%s", sa.c_str());
   }
 
