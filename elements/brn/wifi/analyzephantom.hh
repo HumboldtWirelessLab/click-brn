edec788fe871e57fbf1cfdcb504a33c198a80595
Frank Lange <langefrq@informatik.hu-berlin.de>
2013-04-17 16:25:02 +0200

    Added Features to Phantom Analyzer

--- elements/brn/wifi/analyzephantom.hh
+++ elements/brn/wifi/analyzephantom.hh
@@ -3,22 +3,53 @@
 
 #include <click/element.hh>
 #include <click/etheraddress.hh>
+#include <click/config.h>
+#include <click/error.hh>
+#include <click/confparse.hh>
+#include <click/packet_anno.hh>
+#include <click/straccum.hh>
+#include <click/packet.hh>
+#include <clicknet/wifi.h>
+
+#include "elements/brn/wifi/brnwifi.hh"
 #include "elements/brn/brnelement.hh"
+
+#define INIT      -1
+#define QUEUE_LEN  2
+
+
 
 CLICK_DECLS
 
 
+struct pkt_q_entry {
+  Packet *p;
+  u_int32_t err_type;
+  Timestamp ts;
+};
+
+
+
 class AnalyzePhantom : public BRNElement {
- public:
+
+public:
+  struct pkt_q_entry pkt_q[QUEUE_LEN];
 
   AnalyzePhantom();
   ~AnalyzePhantom();
 
-  const char *class_name() const  { return "AnalyzePhantom"; }
+  const char *class_name() const { return "AnalyzePhantom"; }
   const char *port_count() const { return PORTS_1_1; }
   const char *processing() const { return AGNOSTIC; }
 
-  Packet *simple_action(Packet *);
+  int configure(Vector<String> &conf, ErrorHandler* errh);
+  Packet *simple_action(Packet *p);
+
+
+private:
+  void insert_pkt(Paket *p, u_int32_t err_type);
+  void analyze();
+
 };
 
 
