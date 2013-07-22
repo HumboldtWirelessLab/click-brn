e561ce8ef92a2945f577ae44a5225ef84b119b55
Frank Lange <langefrq@informatik.hu-berlin.de>
2013-07-22 12:18:58 +0200

    DistTimeSync: First usable draft. DTS Handler gets for each the node the timedrift to each other node.

--- elements/brn/wifi/disttimesync.hh
+++ elements/brn/wifi/disttimesync.hh
@@ -18,43 +18,78 @@
 
 #define DISTTIMESYNC_INIT 0
 #define TD_RANDOM -1
-#define ETHER_ADDR_SIZE 6  /* length of a MAC Address in bytes */
+#define ETHER_ADDR_SIZE 6    /* length of a MAC Address in bytes */
 
-#define PKT_BUF_SIZE 16    /* packet buffer size per node */
-#define HST_BUF_SIZE 32    /* host time buffer size per node */
+#define PKT_BUF_SIZE 16      /* packet buffer size per node */
+#define HST_BUF_SIZE 64      /* host time buffer size per node */
 
-#define BUNDLE_SIZE 5      /* no. of pkts to be put in linkprobe pkts */
+#define BUNDLE_SIZE 5        /* no. of pkts to be put in linkprobes */
+
+#define DTS_MAX_OFFSET 100   /* max. artificial offset when simulating */
+#define DTS_MAX_TIMEDRIFT 20 /* max. artificial timedrift when simulating */
 
 CLICK_DECLS
 
 struct PacketSyncInfo {
   u_int8_t  src[ETHER_ADDR_SIZE]; /* sender EtherAddress */
   u_int16_t seq_no;               /* packet sequence no. */
-  u_int64_t host_time;            /* packet k_time from the driver */
-} __attribute((packed));
-/* = 16 Byte */
+  uint64_t host_time;             /* packet k_time from the driver */
+}; /* = 16 Byte */
 
-struct HostTimeDiff {
-  int64_t hst_diff;     /* the actual diff' of host times */
-  Timestamp ts;         /* when did I receive the package */
-  u_int32_t pkt_handle; /* packet handle to identify the package */
+struct HostTimeTuple {
+  uint64_t hst_me;   /* when did the probe rx'ing node receive the orig. pkt */
+  uint64_t hst_nb;   /* when did the probing node receive the package */
+  uint32_t pkt_handle; /* unique packet handle to identify the package */
+}; /* = 20 Byte */
+
+struct DriftInfo {
+  double drift;
+  int64_t offset;
 };
 
 class HostTimeBuf {
 public:
-  struct HostTimeDiff *hst_diffs;
-  u_int32_t hst_idx;
+  struct HostTimeTuple *hst_tpls;
+  uint32_t hst_idx;
+  uint32_t entries;
 
   HostTimeBuf();
   ~HostTimeBuf();
-  int has_pkt(u_int32_t packet_handle);
+
+  int has_pkt(uint32_t packet_handle);
+
+  double get_timedrift();
+  int64_t get_offset();
 };
 
-typedef HashMap<EtherAddress, HostTimeBuf *> TimeDiffTable;
-typedef HashMap<u_int32_t, u_int32_t> PacketIndexTable;
+typedef HashMap<EtherAddress, HostTimeBuf *> TupleBufTable;
+typedef TupleBufTable::const_iterator TBTIter;
+
+typedef HashMap<uint32_t, uint32_t> PacketIndexTable;
+typedef PacketIndexTable::const_iterator PITIter;
 
 class DistTimeSync : public BRNElement {
 public:
+
+  /* artificial time drift and offset used when simulating */
+  int32_t _time_drift; /* 1.23 represented as 123 */
+  int32_t _offset;
+
+  /* size and index of the packet ring buffer used to store rx'd pkts */
+  uint32_t _pkt_buf_size;
+  uint32_t _pkt_buf_idx;
+
+  /* mapping from MAC-Addr to host time tuple buffer */
+  TupleBufTable tbt;
+
+  /* mapping from packet handle to pkt (ring) buffer index */
+  PacketIndexTable pit;
+
+  struct PacketSyncInfo pkt_buf[PKT_BUF_SIZE];
+  BRN2LinkStat *_linkstat;
+
+  uint32_t node_id;
+
   DistTimeSync();
   ~DistTimeSync();
 
@@ -66,29 +101,21 @@ public:
   int initialize(ErrorHandler *);
   Packet *simple_action(Packet *p);
 
+  /* link probe handler */
   int lpSendHandler(char *buf, int size);
-  int lpReceiveHandler(char *buf, int size);
+  int lpReceiveHandler(char *buf, int size, EtherAddress src);
 
+  /* stats handler for access to the calculated time drifts */
   void add_handlers();
   String stats_handler();
 
-  u_int32_t create_packet_handle(u_int16_t seq_no, u_int8_t *src_addr);
-
-  u_int32_t _time_drift;
-  u_int32_t _pkt_buf_size;
-  u_int32_t _pkt_buf_idx;
-
-  TimeDiffTable tdt;
-  PacketIndexTable pit;
-  struct PacketSyncInfo *pkt_buf;
-
-  BRN2LinkStat *_linkstat;
+  /*
+   * creates a unique packet handle consisting of the sequence number and
+   * the last 2 bytes of the sender's MAC-Address:
+   * [seq_no (2) | last 2 MAC (2)]
+   */
+  uint32_t create_pkt_handle(u_int16_t seq_no, u_int8_t *src_addr);
 };
-
-EtherAddress get_sender_addr(struct click_wifi *wh);
-bool hst_buf_has_pkt(HostTimeBuf *hst_buf, u_int32_t packet_handle);
-
-void print_info(Packet *p);
 
 CLICK_ENDDECLS
 
