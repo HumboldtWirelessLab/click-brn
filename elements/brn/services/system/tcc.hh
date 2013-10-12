c330894589071b70f4dff356b669dd7f3a1ba50e
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2013-10-12 08:00:06 +0200

    TCC: add element with code-injection

---
+++ elements/brn/services/system/tcc.hh
@@ -0,0 +1,38 @@
+#ifndef TCC_HH
+#define TCC_HH
+
+#include <click/confparse.hh>
+
+#include "elements/brn/brnelement.hh"
+
+#include "libtcc.h"
+
+CLICK_DECLS
+
+class TCC : public BRNElement {
+public:
+  TCC();
+  ~TCC();
+
+  const char *class_name() const { return "TCC"; }
+  const char *port_count() const { return "1/1"; }
+  const char *processing() const { return AGNOSTIC; }
+
+  int configure(Vector<String> &conf, ErrorHandler *errh);
+  bool can_live_reconfigure() const { return false; }
+  int initialize();
+  void add_handlers();
+
+  Packet* simple_action(Packet *p);
+  void add_code(String code);
+  String stats();
+
+ private:
+
+   TCCState *_tcc_s;
+
+   Packet *(*click_tcc_simple_action)(Packet *p);
+};
+
+CLICK_ENDDECLS
+#endif
