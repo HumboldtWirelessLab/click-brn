0233e6634bfb7609538957eed1448c16f4dc25c2
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2013-11-11 14:17:10 +0100

    MPR: use infos about other nodes to get relays

--- elements/brn/routing/broadcast/flooding/floodingpolicy/mprflooding.cc
+++ elements/brn/routing/broadcast/flooding/floodingpolicy/mprflooding.cc
@@ -18,6 +18,7 @@
 CLICK_DECLS
 
 MPRFlooding::MPRFlooding():
+  _max_metric_to_neighbor(MPR_DEFAULT_NB_METRIC),
   _update_interval(MPR_DEFAULT_UPDATE_INTERVAL),
   _fix_mpr(false)
 {
@@ -64,12 +65,30 @@ MPRFlooding::initialize(ErrorHandler *)
 }
 
 void
-MPRFlooding::set_mpr_header(uint32_t *tx_data_size, uint8_t *txdata)
+MPRFlooding::set_mpr_header(uint32_t *tx_data_size, uint8_t *txdata, EtherAddress *src, int bcast_id)
 {
-  if ((_mpr_forwarder.size() == 0) || //if we have now mpr
-      ((!_fix_mpr) &&                 //or we dont use fixed mprs & need update
-        (Timestamp::now() - _last_set_mpr_call).msecval() > _update_interval)) {
-      set_mpr();
+  if ( bcast_id == -1 ) {
+    if ((_mpr_forwarder.size() == 0) || //if we have now mpr
+        ((!_fix_mpr) &&                 //or we dont use fixed mprs & need update
+         (Timestamp::now() - _last_set_mpr_call).msecval() > _update_interval)) {
+        set_mpr(NULL);
+    }
+  } else {
+    HashMap<EtherAddress,EtherAddress> known_nodes;
+
+    struct Flooding::BroadcastNode::flooding_last_node *last_nodes;
+
+    uint32_t last_nodes_size;
+    last_nodes = _flooding->get_last_nodes(src, bcast_id, &last_nodes_size);
+
+    for ( uint32_t j = 0; j < last_nodes_size; j++ ) {                                     //add node to candidate set if
+      if ( ((last_nodes[j].flags & FLOODING_LAST_NODE_FLAGS_RX_ACKED) != 0) ||             //1. is known as acked
+           ((last_nodes[j].flags & FLOODING_LAST_NODE_FLAGS_FOREIGN_RESPONSIBILITY) != 0)) //2. foreign respons
+          known_nodes.insert(EtherAddress(last_nodes[j].etheraddr),EtherAddress(last_nodes[j].etheraddr));
+    }
+
+    set_mpr(&known_nodes);
+    known_nodes.clear();
   }
 
   if ( (*tx_data_size < (uint32_t)(sizeof(struct click_brn_bcast_extra_data) + (6 * _mpr_forwarder.size()))) || (_mpr_forwarder.size() == 0)) { 
@@ -84,20 +103,29 @@ MPRFlooding::set_mpr_header(uint32_t *tx_data_size, uint8_t *txdata)
     extdat->type = (uint8_t)BCAST_EXTRA_DATA_MPR;
 
     *tx_data_size = sizeof(struct click_brn_bcast_extra_data) + (6 * _mpr_forwarder.size());
-  }  
+  }
 }
 
 
 void
-MPRFlooding::init_broadcast(EtherAddress *, uint32_t, uint32_t *tx_data_size, uint8_t *txdata, Vector<EtherAddress> *, Vector<EtherAddress> *)
+MPRFlooding::init_broadcast(EtherAddress *, uint32_t, uint32_t *tx_data_size, uint8_t *txdata,
+                            Vector<EtherAddress> *unicast_dst, Vector<EtherAddress> *passiveack)
 {
-  set_mpr_header(tx_data_size, txdata);
+  set_mpr_header(tx_data_size, txdata, NULL, -1);
+
+  int n = 1;
+
+  for (Vector<EtherAddress>::iterator i = _mpr_forwarder.begin(); i!=_mpr_forwarder.end(); ++i, n++) {
+    BRN_ERROR("INIT: NODE %d: %s",n,i->unparse().c_str());
+    unicast_dst->push_back(*i);
+    passiveack->push_back(*i);
+  }
 }
 
 bool
-MPRFlooding::do_forward(EtherAddress */*src*/, EtherAddress */*fwd*/, const EtherAddress */*rcv*/, uint32_t /*id*/, bool /*is_known*/, uint32_t forward_count,
-                        uint32_t rx_data_size, uint8_t *rxdata, uint32_t *tx_data_size, uint8_t *txdata,
-                        Vector<EtherAddress> */*unicast_dst*/, Vector<EtherAddress> */*passiveack*/)
+MPRFlooding::do_forward(EtherAddress *src, EtherAddress */*fwd*/, const EtherAddress */*rcv*/, uint32_t id, bool /*is_known*/,
+                        uint32_t forward_count, uint32_t rx_data_size, uint8_t *rxdata, uint32_t *tx_data_size, uint8_t *txdata,
+                        Vector<EtherAddress> *unicast_dst, Vector<EtherAddress> *passiveack)
 {
   EtherAddress ea;
   uint32_t i;
@@ -106,21 +134,21 @@ MPRFlooding::do_forward(EtherAddress */*src*/, EtherAddress */*fwd*/, const Ethe
   if ( forward_count > 0 ) return false;
 
   BRN_DEBUG("do_fwd: %d", rx_data_size);
-  
+
   if ( rx_data_size == 0 ) fwd = true;
   else {
     for(i = 0; (i < rx_data_size) && (fwd == false);) {
-      
+
       struct click_brn_bcast_extra_data *extdat = (struct click_brn_bcast_extra_data *)&(rxdata[i]);
 
       BRN_DEBUG("i: %d Type: %d Size: %d",i, (uint32_t)extdat->type, (uint32_t)extdat->size);
-      
+
       if ( extdat->type == BCAST_EXTRA_DATA_MPR ) {
         BRN_DEBUG("Found MPR stuff: Size: %d",extdat->size);
-    
+
         uint32_t rxdata_idx = i + sizeof(struct click_brn_bcast_extra_data);
         uint32_t mpr_data_idx = sizeof(struct click_brn_bcast_extra_data);
-        
+
         for(;mpr_data_idx < extdat->size; mpr_data_idx += 6, rxdata_idx += 6 ) {
           ea = EtherAddress(&(rxdata[rxdata_idx]));
           if ( _me->isIdentical(&ea) || (ea == brn_etheraddress_broadcast)) {
@@ -140,7 +168,13 @@ MPRFlooding::do_forward(EtherAddress */*src*/, EtherAddress */*fwd*/, const Ethe
   }
 
   if (fwd) { //i'm a mpr
-    set_mpr_header(tx_data_size, txdata);
+    set_mpr_header(tx_data_size, txdata, src, id /*-1*/);
+
+    for (Vector<EtherAddress>::iterator i = _mpr_forwarder.begin(); i!=_mpr_forwarder.end(); ++i) {
+      unicast_dst->push_back(*i);
+      passiveack->push_back(*i);
+    }
+
     return true;
   }
 
@@ -154,14 +188,14 @@ MPRFlooding::policy_id()
 }
 
 void
-MPRFlooding::set_mpr()
+MPRFlooding::set_mpr(HashMap<EtherAddress,EtherAddress> *known_nodes)
 {
   Vector<EtherAddress> neighbors;
 
-  HashMap<EtherAddress, int> neighbour_map; 
-  HashMap<EtherAddress, int> uncovered; 
+  HashMap<EtherAddress, int> neighbour_map;
+  HashMap<EtherAddress, int> uncovered;
 
-  HashMap<EtherAddress, EtherAddress> mpr_nodes; 
+  HashMap<EtherAddress, EtherAddress> mpr_nodes;
 
   uint8_t *adj_mat;
   uint16_t adj_mat_size = 16;
@@ -178,7 +212,8 @@ MPRFlooding::set_mpr()
   //get neighbours
   //
   const EtherAddress *me = _me->getMasterAddress();
-  _fhelper->get_filtered_neighbors(*me, neighbors);
+  _fhelper->get_filtered_neighbors(*me, neighbors, _max_metric_to_neighbor);
+  if ( known_nodes != NULL ) remove_finished_neighbours(&neighbors, known_nodes);
 
   if (neighbors.size() == 0) {
     BRN_DEBUG("No neighbours, so no mprs.");
@@ -204,7 +239,8 @@ MPRFlooding::set_mpr()
   for( int n_i = 0; n_i < count_one_hop_nbs; n_i++) { // iterate over all my neighbors
 
     Vector<EtherAddress> nb_neighbors;               // the neighbors of my neighbors
-    _fhelper->get_filtered_neighbors(neighbors[n_i], nb_neighbors);
+    _fhelper->get_filtered_neighbors(neighbors[n_i], nb_neighbors, _max_metric_to_neighbor);
+    if ( known_nodes != NULL ) remove_finished_neighbours(&nb_neighbors, known_nodes);
 
     for( int n_j = 0; n_j < nb_neighbors.size(); n_j++) { // iterate over all my neighbors neighbours
 
@@ -216,36 +252,36 @@ MPRFlooding::set_mpr()
         neighbour_map.insert(nb_neighbors[n_j],adj_mat_index);
         uncovered.insert(nb_neighbors[n_j],1);
         neighbors.push_back(nb_neighbors[n_j]);
-        
+
         if ( adj_mat_used == adj_mat_size ) {
           adj_mat_size *= 2;
           uint8_t *new_adj_mat = new uint8_t[adj_mat_size*adj_mat_size]; //Matrix
           memset(new_adj_mat, 0, adj_mat_size*adj_mat_size);
-          
+
           //copy old matrix
           for ( int i = 0; i < adj_mat_index; i++ )
             for ( int j = 0; j < adj_mat_index; j++ )
               new_adj_mat[i*adj_mat_size + j] = adj_mat[i*adj_mat_used + j];
-            
+
           delete adj_mat;
           adj_mat = new_adj_mat;
         }
-      
+
         adj_mat_used++;
-        
+
       } else {
         adj_mat_index = neighbour_map.find(nb_neighbors[n_j]);
-        if ( adj_mat_index >= count_one_hop_nbs ) {           //is a two hop neighbour
-          int *cov_count = uncovered.findp(nb_neighbors[n_j]);
-          (*cov_count)++;
+        if ( adj_mat_index >= count_one_hop_nbs ) {            //is a two hop neighbour
+          int *cov_count = uncovered.findp(nb_neighbors[n_j]); //this two hop neighbors is covered by
+          (*cov_count)++;                                      //one more 1-hop-neighbor
         }
       }
-        
+
       adj_mat[n_i*adj_mat_size + adj_mat_index] = 1;
       adj_mat[adj_mat_index*adj_mat_size + n_i] = 1;
     }
   }
-  
+
   //
   // DEBUG
   //
@@ -261,14 +297,14 @@ MPRFlooding::set_mpr()
 
     click_chatter("%s", sa.take_string().c_str());
   }
-  
+
   //found one hop neighbours who are single cov for 2-hop-neighbour
-  
+
   for( int i = count_one_hop_nbs; i < adj_mat_used; i++) {  //check all 2-hop neighbours
     EtherAddress ea = neighbors[i];
-    
+
     if ( uncovered.findp(ea) == NULL ) continue;            //already coverd
-    
+
     uint8_t n = uncovered.find(ea);
     if ( n == 1 ) {                                         //covered only once
       BRN_DEBUG("Covered by one: %s",ea.unparse().c_str());
@@ -281,22 +317,21 @@ MPRFlooding::set_mpr()
           conn_count++;
         }
       }
-        
+
       if ( conn_count == 1 ) {                             // 2 hop node only covered by onre hop node one_hop_neighbour
         uncovered.erase(ea);                               //node is coverd by one node (mpr)
         _mpr_forwarder.push_back(neighbors[one_hop_neighbour]); //node is mpr
         mpr_nodes.insert(neighbors[one_hop_neighbour],neighbors[one_hop_neighbour]);
-        
+
         //delete edges to node
         for ( int x = 0; x < adj_mat_size;x++) {
           adj_mat[x*adj_mat_size + i] = 0;
           adj_mat[i*adj_mat_size + x] = 0;
         }
-            
-        
+
         for( int j = count_one_hop_nbs; j < adj_mat_used; j++ ) {  //check for other nodes coverd by one hop node
           if ( adj_mat[j*adj_mat_size + one_hop_neighbour] == 1 ) {
-            
+
             //every node coverd by mpr can be deleted
             for ( int x = 0; x < adj_mat_size;x++) {
               adj_mat[x*adj_mat_size + j] = 0;
@@ -313,7 +348,7 @@ MPRFlooding::set_mpr()
       BRN_DEBUG("Covered by %d nodes",(int)n);
     }
   }
-  
+
   for( int m = 0; m < _mpr_forwarder.size(); m++ ) {
     BRN_DEBUG("MPR: %s", _mpr_forwarder[m].unparse().c_str());
   }
@@ -330,38 +365,38 @@ MPRFlooding::set_mpr()
         BRN_DEBUG("Uncovered node: %s",neighbors[g].unparse().c_str());
       }
     }
-      
+
     for( int i = 0; i < count_one_hop_nbs; i++) {  //check all 1-hop neighbours
       EtherAddress ea = neighbors[i];
       if ( mpr_nodes.findp(ea) != NULL ) continue;
-      
+
       uint16_t cnt_2hop_nb = 0;
-      
+
       BRN_DEBUG("Non mpr node: %s %d %d",ea.unparse().c_str(),max_id,cnt_2hop_nb);
-      
+
       for( int j = count_one_hop_nbs; j < adj_mat_used; j++ ) {
         if ( adj_mat[j*adj_mat_size + i] == 1 ) cnt_2hop_nb++;
         else if ( adj_mat[i*adj_mat_size + j] == 1 ) cnt_2hop_nb++;
       }
-      
+
       if ( cnt_2hop_nb > max_count ) {
         max_ea = ea;
         max_count = cnt_2hop_nb;
         max_id = i;
       }
     }
-    
+
     BRN_DEBUG("MaxCount: %d", max_count);
-    
+
     if ( max_count == 0 ) {
       BRN_DEBUG("Error");
     } else {
       _mpr_forwarder.push_back(max_ea); //node is mpr
       mpr_nodes.insert(max_ea,max_ea);
-      
+
       for( int j = count_one_hop_nbs; j < adj_mat_used; j++ ) {  //check for other nodes coverd by one hop node
         if ( adj_mat[j*adj_mat_size + max_id ] == 1 ) {
-            
+
           for ( int x = 0; x < adj_mat_size;x++) {             //2hop node is coverd: delete edges
             adj_mat[x*adj_mat_size + j] = 0;
             adj_mat[j*adj_mat_size + x] = 0;
@@ -373,11 +408,19 @@ MPRFlooding::set_mpr()
         }
       }
     }
-    
+
     unc_s--;
   }
-  
+
   delete[] adj_mat;
+}
+
+void
+MPRFlooding::remove_finished_neighbours(Vector<EtherAddress> *neighbors, HashMap<EtherAddress, EtherAddress> *known_nodes)
+{
+  for( int i = neighbors->size()-1; i >= 0; i--)
+    if ( known_nodes->findp((*neighbors)[i]) != NULL )
+       neighbors->erase(neighbors->begin() + i);
 }
 
 int
@@ -408,12 +451,6 @@ MPRFlooding::set_mpr_forwarder(const String &in_s)
   return result;
 }
 
-int
-MPRFlooding::set_mpr_destination(const String &in_s)
-{
-  return set_mpr_vector(in_s, &_mpr_unicast);
-}
-
 /*******************************************************************************************/
 /************************************* H A N D L E R ***************************************/
 /*******************************************************************************************/
@@ -422,9 +459,9 @@ String
 MPRFlooding::flooding_info(void)
 {
   StringAccum sa;
-  
+
   sa << "<mprflooding node=\"" << _me->getMasterAddress()->unparse().c_str() << "\" >\n";
-  
+
   sa << "\t<mprset size=\"" << _mpr_forwarder.size() << "\" >\n";
   for( int i = 0; i < _mpr_forwarder.size(); i++ ) {
     sa << "\t\t<mpr node=\"" << _mpr_forwarder[i].unparse().c_str() << "\" />\n";
@@ -436,7 +473,7 @@ MPRFlooding::flooding_info(void)
     sa << "\t\t<neighbour node=\"" << _neighbours[i].unparse().c_str() << "\" />\n";
   }
   sa << "\t</neighbourset>\n";
-  
+
   sa << "</mprflooding>\n";
 
   return sa.take_string();
@@ -470,10 +507,9 @@ write_param(const String &in_s, Element *e, void *vparam, ErrorHandler */*errh*/
   switch ((uintptr_t) vparam)
   {
     case H_MPRFLOODING_FORWARDER : return ( mprfl->set_mpr_forwarder(in_s) );
-    case H_MPRFLOODING_DESTINATION : return ( mprfl->set_mpr_destination(in_s) );
     case H_MPRFLOODING_GET_MPR : {
-                                    mprfl->set_mpr();
-				    return 0;
+                                    mprfl->set_mpr(NULL);
+                                    return 0;
                                  }
     default: return 0;
   }
@@ -488,9 +524,8 @@ void MPRFlooding::add_handlers()
   add_read_handler("flooding_info", read_param , (void *)H_FLOODING_INFO);
 
   add_write_handler("forwarder", write_param, (void *) H_MPRFLOODING_FORWARDER);
-  add_write_handler("destination", write_param, (void *) H_MPRFLOODING_DESTINATION);
   add_write_handler("mpr_algo", write_param, (void *) H_MPRFLOODING_GET_MPR);
-  
+
 }
 
 CLICK_ENDDECLS
--- elements/brn/routing/broadcast/flooding/floodingpolicy/mprflooding.hh
+++ elements/brn/routing/broadcast/flooding/floodingpolicy/mprflooding.hh
@@ -8,6 +8,7 @@
 CLICK_DECLS
 
 #define MPR_DEFAULT_UPDATE_INTERVAL 5000
+#define MPR_DEFAULT_NB_METRIC         -1  /* Use metric of floodinghelper */
 
 class MPRFlooding : public FloodingPolicy
 {
@@ -35,18 +36,21 @@ class MPRFlooding : public FloodingPolicy
     const char *floodingpolicy_name() const { return "MPRFlooding"; }
     int floodingpolicy_id() const { return POLICY_ID_MPR; };
 
-    void set_mpr_header(uint32_t *tx_data_size, uint8_t *txdata);
+    void set_mpr_header(uint32_t *tx_data_size, uint8_t *txdata, EtherAddress *src, int bcast_id);
 
     bool do_forward(EtherAddress *src, EtherAddress *fwd, const EtherAddress *rcv, uint32_t id, bool is_known, uint32_t forward_count,
                     uint32_t rx_data_size, uint8_t *rxdata, uint32_t *tx_data_size, uint8_t *txdata,
                     Vector<EtherAddress> *unicast_dst, Vector<EtherAddress> *passiveack);
-    void init_broadcast(EtherAddress *, uint32_t, uint32_t *, uint8_t *,
-		       Vector<EtherAddress> *, Vector<EtherAddress> *);
+
+    void init_broadcast(EtherAddress *, uint32_t, uint32_t *, uint8_t *, Vector<EtherAddress> *, Vector<EtherAddress> *);
+
     int policy_id();
 
     String flooding_info(void);
 
-    void set_mpr();
+    void set_mpr(HashMap<EtherAddress,EtherAddress> *known_nodes);
+
+    void remove_finished_neighbours(Vector<EtherAddress> *neighbors, HashMap<EtherAddress, EtherAddress> *known_nodes);
 
     int set_mpr_vector(const String &in_s, Vector<EtherAddress> *ea_vector);
     int set_mpr_forwarder(const String &in_s);
@@ -63,7 +67,6 @@ class MPRFlooding : public FloodingPolicy
     bool _fix_mpr;
 
     Vector<EtherAddress> _mpr_forwarder;
-    Vector<EtherAddress> _mpr_unicast;
     Vector<EtherAddress> _neighbours;
 };
 
