5d639050bdfc6778081ba30ee69f1f0f868cda6d
Robert Döring <rdoering.info@gmail.com>
2013-11-25 22:17:37 +0100

    Added TTL and cleaned code.

--- elements/brn/topology/topology_dibadawn_packet.cc
+++ elements/brn/topology/topology_dibadawn_packet.cc
@@ -35,14 +35,15 @@ CLICK_DECLS
 
 
 #pragma pack(1)
-struct dibadawn_packet
+struct DibadawnPacketStruct
 {
   uint8_t version : 4;
   uint8_t reserve : 2;
   uint8_t type : 2;
 
   uint8_t id[DibadawnSearchId::length];
-  uint8_t src[DibadawnPacket::LengthOfForwardedBy];
+  uint8_t forwaredBy[DibadawnPacket::LengthOfAddr];
+  uint8_t treeParent[DibadawnPacket::LengthOfAddr];
   uint8_t ttl;
 };
 
@@ -51,23 +52,32 @@ DibadawnPacket::DibadawnPacket()
   setVersion();
 }
 
-DibadawnPacket::DibadawnPacket(const Packet *packet)
+DibadawnPacket::DibadawnPacket(const Packet *brn_packet)
 {
+  DibadawnPacketStruct *packet;
+  packet = (struct DibadawnPacketStruct *) brn_packet->data();
+  
+  memcpy(forwardedBy, packet->forwaredBy, sizeof (forwardedBy));
+  memcpy(treeParrent, packet->treeParent, sizeof (treeParrent));
+  ttl = packet->ttl;
+  isForward = (packet->type & 0x03) != 0;
+  searchId.setByPointerTo10BytesOfData(packet->id);
 }
 
 DibadawnPacket::DibadawnPacket(DibadawnSearchId *id, const EtherAddress* sender_addr, bool is_forward)
 {
   setVersion();
-  setSearchId(id);
+  searchId = *id;
   setTreeParent(NULL);
   setForwaredBy(sender_addr);
   isForward = is_forward;
+  ttl = 255; // TODO: Does this makes sense?
 }
 
 bool DibadawnPacket::isValid(const Packet *brn_packet)
 {
-  struct dibadawn_packet *packet;
-  packet = (struct dibadawn_packet *) brn_packet->data();
+  DibadawnPacketStruct *packet;
+  packet = (struct DibadawnPacketStruct *) brn_packet->data();
   return (packet->version == 1);
 }
 
@@ -76,15 +86,17 @@ void DibadawnPacket::setVersion()
   this->version = 1;
 }
 
-void DibadawnPacket::setSearchId(DibadawnSearchId* id)
-{
-  this->searchId = *id;
-}
-
 void DibadawnPacket::setForwaredBy(const EtherAddress* sender_addr)
 {
-  const uint8_t *bytes = sender_addr->data();
-  memcpy(forwardedBy, bytes, sizeof (forwardedBy));
+  if (sender_addr == NULL)
+  {
+    memset(forwardedBy, 0x00, sizeof (forwardedBy));
+  }
+  else
+  {
+    const uint8_t *bytes = sender_addr->data();
+    memcpy(forwardedBy, bytes, sizeof (forwardedBy));
+  }
 }
 
 void DibadawnPacket::setTreeParent(const EtherAddress* sender_addr)
@@ -102,19 +114,21 @@ void DibadawnPacket::setTreeParent(const EtherAddress* sender_addr)
 
 WritablePacket* DibadawnPacket::getBrnPacket()
 {
-  dibadawn_packet content;
+  DibadawnPacketStruct content;
   content.version = version & 0x0F;
   content.type = isForward ? 0 : 1;
   memcpy(content.id, searchId.PointerTo10BytesOfData(), sizeof (content.id));
-  memcpy(content.src, forwardedBy, sizeof (content.src));
-  content.ttl = 100; // TODO: Does this makes sense?
+  memcpy(content.forwaredBy, forwardedBy, sizeof (content.forwaredBy));
+  content.ttl = ttl; 
 
+  LOG("Sizeof(DibadawnPacketStruct) %d", sizeof(DibadawnPacketStruct));
+  
   uint32_t sizeof_head = 128;
-  uint32_t sizeof_data = sizeof (content);
   uint32_t sizeof_tail = 32;
   WritablePacket *packet = WritablePacket::make(
       sizeof_head,
-      &content, sizeof_data,
+      &content, 
+      sizeof(content),
       sizeof_tail);
 
   WritablePacket *brn_packet = BRNProtocol::add_brn_header(
