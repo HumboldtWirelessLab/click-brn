5c3b65ae183316adfc1a9b5bfe5c10f7f9752eda
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2014-12-08 18:12:23 +0100

    Flooding: fix compile error

--- elements/brn/routing/broadcast/flooding/floodingprenegotiation.cc
+++ elements/brn/routing/broadcast/flooding/floodingprenegotiation.cc
@@ -40,13 +40,15 @@ CLICK_DECLS
 
 FloodingPrenegotiation::FloodingPrenegotiation():
   _seq(0),
-  _max_ids_per_packet(8)
+  _max_ids_per_packet(DEFAULT_MAX_FLOODING_IDS_PER_PACKET),
+  _node_timeout(DEFAULT_FLOODING_NEIGHBOURS_TIMEOUT)
 {
   BRNElement::init();
 }
 
 FloodingPrenegotiation::~FloodingPrenegotiation()
 {
+  _neighbour_map.clear();
 }
 
 int
@@ -90,6 +92,8 @@ FloodingPrenegotiation::initialize(ErrorHandler */*errh*/)
 int
 FloodingPrenegotiation::lpSendHandler(char *buffer, int size)
 {
+  Timestamp now = Timestamp::now();
+
   if (_flooding_db->_recv_cnt.size() == 0) {
     BRN_INFO("No Flooding");
     return 0;
@@ -123,7 +127,7 @@ FloodingPrenegotiation::lpSendHandler(char *buffer, int size)
       id_index++;
       sum += bcn->_bcast_snd_list[i];
 
-      BRN_DEBUG("Add ID %d tx %d",bcn->_bcast_id_list[i], bcn->_bcast_snd_list[i]);
+      BRN_DEBUG("Add ID %d tx %d", bcn->_bcast_id_list[i], bcn->_bcast_snd_list[i]);
   }
 
   if ( sum < 5 ) return 0;
@@ -131,17 +135,37 @@ FloodingPrenegotiation::lpSendHandler(char *buffer, int size)
   fli->no_src_ids = (uint8_t)id_index;
   fli->sum_transmissions = htons(sum);
 
-  /* Fill up macs */
+  int space_pointer = sizeof(struct fooding_link_information) + id_index*sizeof(uint16_t);
 
-  /* Fill up RX PROB */
+  NodeAgeMapIter iter = _neighbour_map.begin();
 
-  return sizeof(struct fooding_link_information) + id_index*sizeof(uint16_t);
+  while (((space_pointer + (7 * sizeof(uint8_t))) <= (u_int32_t)size) && iter.live() ) { //enough space fo rmac and rx probability
+    /* Fill up macs and RX PROB */
+    Timestamp ts = iter.value();                                                         //get timestamp (last rx)
+
+    if ( (now-ts).msecval() <= _node_timeout ) {                                         //timeout? no,..
+      EtherAddress ea = iter.key();                                                      //get etheraddr...
+
+      uint16_t pdr = _link_table->get_link_metric(ea, *_linkstat->_dev->getEtherAddress()); //..and pdr
+      if ( pdr != BRN_LT_INVALID_LINK_METRIC ) {                                            //is valid pdr?
+        memcpy(&(buffer[space_pointer]), ea.data(), 6);                                     //Yes, so push etheraddr into packet
+        buffer[space_pointer + 6] = (uint8_t)(pdr >> 6);                                    //...and pdr too.
+        space_pointer += 7 * sizeof(uint8_t);                                               //set pointer to next position in packet
+        fli->no_rx_nodes++;                                                                 //inc no nodes (we now have one more in p)
+      }
+    }
+
+    iter++;
+  }
+
+  return space_pointer;
 }
 
 int
-FloodingPrenegotiation::lpReceiveHandler(EtherAddress *ea, char *buffer, int /*size*/)
+FloodingPrenegotiation::lpReceiveHandler(EtherAddress *ea, char *buffer, int size)
 {
   BRN_DEBUG("Got Flooding Info.: %s", ea->unparse().c_str());
+  _neighbour_map.insert(*ea,Timestamp::now());
 
   struct fooding_link_information *fli = (struct fooding_link_information*)buffer;
 
@@ -159,7 +183,7 @@ FloodingPrenegotiation::lpReceiveHandler(EtherAddress *ea, char *buffer, int /*s
 
   for(uint32_t i = id_index; i > 0; i--) {
     BRN_DEBUG("ID: %d",ntohs(id_pointer[i-1]));
-    struct Click::BroadcastNode::flooding_node_info* fni = bcn->get_node_info(ntohs(id_pointer[i-1]), ea);
+    struct BroadcastNode::flooding_node_info* fni = bcn->get_node_info(ntohs(id_pointer[i-1]), ea);
     if (fni) rx_sum += fni->received_cnt;
   }
 
@@ -171,11 +195,29 @@ FloodingPrenegotiation::lpReceiveHandler(EtherAddress *ea, char *buffer, int /*s
                                               rx_prob << 6, LINK_UPDATE_LOCAL_ACTIVE, false);
 
   BRN_DEBUG("Prob: %d", rx_prob);
-  /* Get macs */
 
-  /* Get RX PROB */
+  BRN_DEBUG("Includes: %d",fli->no_rx_nodes);
 
-  return sizeof(struct fooding_link_information) + id_index*sizeof(uint16_t);
+  int space_pointer = sizeof(struct fooding_link_information) + id_index*sizeof(uint16_t);
+
+  for(uint32_t i = 0; i < fli->no_rx_nodes; i++) {
+    /* Get macs */
+    /* Get RX PROB */
+    EtherAddress rx_ea = EtherAddress((unsigned char*)&(buffer[space_pointer]));
+    uint8_t pdr = buffer[space_pointer+6];
+
+    if ( pdr > 0 ) _link_table->update_link( rx_ea, *ea, _seq, 1,
+                                            pdr << 6, LINK_UPDATE_LOCAL_ACTIVE, false);
+
+    space_pointer += 7 * sizeof(uint8_t);
+
+    BRN_DEBUG("%s : %d", rx_ea.unparse().c_str(),(uint32_t)pdr);
+
+  }
+
+  assert(space_pointer == size);
+
+  return space_pointer;
 }
 
 //-----------------------------------------------------------------------------
