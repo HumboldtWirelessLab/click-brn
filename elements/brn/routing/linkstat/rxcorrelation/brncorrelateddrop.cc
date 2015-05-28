cb5528282b0b6cbdf97ec240b8cc23060a8b126d
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2015-05-28 17:01:55 +0200

    CorrelatedDrop: Drop p only for given source, bits of pattern int are used

--- elements/brn/routing/linkstat/rxcorrelation/brncorrelateddrop.cc
+++ elements/brn/routing/linkstat/rxcorrelation/brncorrelateddrop.cc
@@ -10,7 +10,8 @@
 CLICK_DECLS
 
 BRNCorrelatedDrop::BRNCorrelatedDrop()
-  :_drop_pattern(0),
+  :_dropped_source(),
+   _drop_pattern(0),
    _packet(0),
    _skew(0)
 {
@@ -24,9 +25,9 @@ int
 BRNCorrelatedDrop::configure(Vector<String> &conf, ErrorHandler *errh)
 {
   if (cp_va_kparse(conf, this, errh,
-      "PATTERN", cpkP+cpkM, cpInteger, &_drop_pattern,
+      "PATTERN", cpkP+cpkM, cpUnsigned, &_drop_pattern,
       "SKEW", cpkP+cpkM, cpInteger, &_skew,
-//      "DEBUG", cpkP, cpInteger, &_debug,
+      "ETHERADDRESS", cpkP, cpEtherAddress, &_dropped_source,
       cpEnd) < 0)
     return -1;
 
@@ -36,10 +37,15 @@ BRNCorrelatedDrop::configure(Vector<String> &conf, ErrorHandler *errh)
 Packet *
 BRNCorrelatedDrop::simple_action(Packet *p)
 {
-  _packet++;
+  click_ether *ether = (click_ether *) p->data();
+  EtherAddress src = EtherAddress(ether->ether_shost);
+  //EtherAddress src = BRNPacketAnno::src_ether_anno(p);
+
+  //if we only want to drop packets from a given source (_dropped_source.is_set()) than we just drop packets from src
+  if ((_dropped_source.is_set()) && (_dropped_source != src)) return p;
 
   if ( _drop_pattern != 0 ) {
-    if ( ( _skew + _packet ) % _drop_pattern == 0 ) {
+    if ( ((_drop_pattern >> ((_skew + (_packet++))%32)) & 1) == 1 ) {
       p-> kill();
       return NULL;
     }
